fb6a0511dab1a6e12b4c611b16c01f84dc88db5b
Daniel Jane <danij4ne@gmail.com>
2025-11-28 21:16:09 +0100

    Add DevAcademy database creation script

---
+++ sql/01_create_database.sql
@@ -0,0 +1,14 @@
+
+-- 1. DATABASE CREATION AND BASIC ADMINISTRATION
+
+-- Drop test database if it exists
+DROP DATABASE IF EXISTS devacademy_test;
+
+-- (Optional) Drop main database if it exists to start from a clean state
+-- DROP DATABASE IF EXISTS devacademy;
+
+-- Create main database
+CREATE DATABASE IF NOT EXISTS devacademy;
+
+-- Select database
+USE devacademy;
